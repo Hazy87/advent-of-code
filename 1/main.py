21c822d326c6d5a481c77a5eb49b0b9513f72dd1
Alex <meandu229@protonmail.com>
2021-12-02 22:34:06 +0000

    Day 1 part 2

--- 1/main.py
+++ 1/main.py
@@ -1,10 +1,11 @@
 import os
 with open('input.txt') as file:
     array = file.readlines()
-    previous = int(array[0]);
-    counter = 0;
-    for line in array:
-      if(previous < int(line)):
+    previous = int(array[0]) + int(array[1]) + int(array[2])
+    counter = 0
+    for index in range(len(array)-2):
+      new = (int(array[index]) + int(array[index+1]) + int(array[index+2]))
+      if(previous < new):
           counter = counter + 1
-      previous = int(line)
+      previous = new
     print(counter)
