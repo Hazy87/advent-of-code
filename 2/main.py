e3f10ebaabde8c4672c2173c40749505febe7aa2
Alex <meandu229@protonmail.com>
2021-12-02 22:49:48 +0000

    Day 2 part 2

--- 2/main.py
+++ 2/main.py
@@ -3,13 +3,15 @@ with open('input.txt') as file:
     array = file.readlines()
     horizontalPosition = 0
     depth = 0
+    aim = 0
     for instruction in array:
        instructions = instruction.split(" ")
        if(instructions[0] == "up"):
-         depth = depth - int(instructions[1])
+         aim = aim - int(instructions[1])
        if(instructions[0] == "down"):
-         depth = depth + int(instructions[1])
+         aim = aim + int(instructions[1])
        if(instructions[0] == "forward"):
          horizontalPosition = horizontalPosition + int(instructions[1])
+         depth = depth + (aim * int(instructions[1]))
 
     print(depth * horizontalPosition)
