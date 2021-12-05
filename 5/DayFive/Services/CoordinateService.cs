054facd95760324466750627166b43aefc913832
Alex <meandu229@protonmail.com>
2021-12-05 10:44:25 +0000

    Day 5 part 1.9

--- 5/DayFive/Services/CoordinateService.cs
+++ 5/DayFive/Services/CoordinateService.cs
@@ -5,7 +5,7 @@ public class CoordinateService : ICoordinateService
     public List<Coordinate> GetCoordinateListFromRange(Coordinate start, Coordinate end)
     {
         var returnList = new List<Coordinate>();
-        if (start.X == end.X)
+        if (start.X == end.X && start.Y != end.Y)
         {
             var lowestY = start.Y > end.Y ? end.Y : start.Y;
             var highestY = start.Y < end.Y ? end.Y : start.Y;
@@ -14,16 +14,52 @@ public class CoordinateService : ICoordinateService
                 returnList.Add(new Coordinate { X = start.X, Y = i });
             }
         }
+        else if (start.Y == end.Y && start.X != end.X)
+        {
+            var lowestX = start.X > end.X ? end.X : start.X;
+            var highestX = start.X < end.X ? end.X : start.X;
+            for (int i = lowestX; i <= highestX; i++)
+            {
+                returnList.Add(new Coordinate { X = i, Y = start.Y });
+            }    
+        }
+        else if(start.X == start.Y && end.Y == end.X)
+        {
+            var lowestX = start.X > end.X ? end.X : start.X;
+            var highestX = start.X < end.X ? end.X : start.X;
+            for (int i = lowestX; i <= highestX; i++)
+            {
+                returnList.Add(new Coordinate { X = i, Y = i });
+            }
+        }
+        else if (start.X > end.X && start.Y > end.Y)
+        {
+            var counter = 0;
+            for (int i = start.X; i >= end.X; i--)
+            {
+                returnList.Add(new Coordinate { X = i, Y = start.Y - counter });
+                counter++;
+            }
+        }
+
+        else if (start.X < end.X && start.Y < end.Y)
+        {
+            var counter = 0;
+            for (int i = start.Y; i <= end.Y; i++)
+            {
+                returnList.Add(new Coordinate { X = start.X + counter, Y = i});
+                counter++;
+            }
+        }
         else
         {
-            if (start.Y == end.Y)
+            var lowestX = start.X > end.X ? end.X : start.X;
+            var highestX = start.X < end.X ? end.X : start.X;
+            var counter = 0;
+            for (int i = lowestX; i <= highestX; i++)
             {
-                var lowestX = start.X > end.X ? end.X : start.X;
-                var highestX = start.X < end.X ? end.X : start.X;
-                for (int i = lowestX; i <= highestX; i++)
-                {
-                    returnList.Add(new Coordinate { X = i, Y = start.Y });
-                }
+               returnList.Add(new Coordinate { X = i, Y =  end.Y - counter});
+               counter++;
             }
         }
         return returnList;
--- 5/TestProject1/UnitTest1.cs
+++ 5/TestProject1/UnitTest1.cs
@@ -1,6 +1,6 @@
 using DayFive.Services;
 using Xunit;
-
+using System.Linq;
 namespace TestProject1
 {
     public class UnitTest1
@@ -16,6 +16,75 @@ namespace TestProject1
         {
             var coords = _service.GetCoordinateListFromRange(new Coordinate() { X = 1, Y = 1 }, new() { X = 3, Y = 1 });
             Assert.Equal(3, coords.Count);
+        }
+        [Fact]
+        public void Correct_CoordCountBackY()
+        {
+            var coords = _service.GetCoordinateListFromRange(new Coordinate() { X = 3, Y = 1 }, new() { X = 3, Y = 10 });
+            Assert.Equal(10, coords.Count);
+        }
+        [Fact]
+        public void Correct_CoordCountY()
+        {
+            var coords = _service.GetCoordinateListFromRange(new Coordinate() { X = 3, Y = 10}, new() { X = 3, Y = 1 });
+            Assert.Equal(10, coords.Count);
+        }
+        [Fact]
+        public void Correct_CoordCountBack()
+        {
+            var coords = _service.GetCoordinateListFromRange(new Coordinate() { X = 3, Y = 1 }, new() { X = 1, Y = 1 });
+            Assert.Equal(3, coords.Count);
+        }
+        [Fact]
+        public void Correct_CoordCountDiagonal()
+        {
+            var coords = _service.GetCoordinateListFromRange(new Coordinate() { X = 9, Y = 7 }, new() { X = 7, Y = 9 });
+            Assert.Equal(3, coords.Count);
+            Assert.Equal(1, coords.Where(x => x.X == 9 && x.Y == 7).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 8 && x.Y == 8).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 7 && x.Y == 9).Count());
+        }
+
+        [Fact]
+        public void Correct_CoordCountOtherDiagonal()
+        {
+            var coords = _service.GetCoordinateListFromRange(new Coordinate() { X =1, Y = 1}, new() { X = 3, Y = 3});
+            Assert.Equal(3, coords.Count);
+            Assert.Equal(1, coords.Where(x => x.X == 1 && x.Y == 1).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 2 && x.Y == 2).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 3 && x.Y == 3).Count());
+        }
+        [Fact]
+        public void Correct_CoordCountOtherDiagonalBack()
+        {
+            var coords = _service.GetCoordinateListFromRange(new Coordinate() { X = 3, Y = 3 }, new() { X = 1, Y = 1 });
+            Assert.Equal(3, coords.Count);
+            Assert.Equal(1, coords.Where(x => x.X == 1 && x.Y == 1).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 2 && x.Y == 2).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 3 && x.Y == 3).Count());
+        }
+        [Fact]
+        public void Correct_CoordCountOtherDiagonalAnoying()
+        {
+            var coords = _service.GetCoordinateListFromRange(new Coordinate() { X = 6, Y = 4}, new() { X = 2, Y = 0 });
+            Assert.Equal(5, coords.Count);
+            Assert.Equal(1, coords.Where(x => x.X == 6 && x.Y == 4).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 5 && x.Y == 3).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 4 && x.Y == 2).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 3 && x.Y == 1).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 2 && x.Y == 0).Count());
+        }
+
+        [Fact]
+        public void Correct_CoordCountOtherDiagonalAnoyingback()
+        {
+            var coords = _service.GetCoordinateListFromRange(new Coordinate() { X = 2, Y = 0 }, new() { X = 6, Y = 4 });
+            Assert.Equal(5, coords.Count);
+            Assert.Equal(1, coords.Where(x => x.X == 6 && x.Y == 4).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 5 && x.Y == 3).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 4 && x.Y == 2).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 3 && x.Y == 1).Count());
+            Assert.Equal(1, coords.Where(x => x.X == 2 && x.Y == 0).Count());
         }
     }
 }
